ecbb3c3e8c2225e11a74a733864cb36636baa1e6
Todd Wickizer <toddwickizer@gmail.com>
2017-03-29 11:43:02 -0700

    Init

---
+++ cron_attempt.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# ensure local directory is created $HOME/cyverse_tmp/completed
+# foreach directory found in $HOME/cyverse_tmp/staging/ (they should be named by date)
+# irsync the folder
+# if status successful, move to $HOME/cyverse_tmp/completed/
+
+# declare the locations of these executables and directories
+IRSYNC=/usr/bin/irsync
+FIND=/usr/bin/find
+# DTL is the location of where you want the files on your IRODS server
+DELAYED_TRANSFER_LOCATION=delayed_transfers
+DTL=$DELAYED_TRANSFER_LOCATION
+
+
+DONE_LOCATION=${HOME}/cyverse_tmp/completed
+LOG_LOCATION=${HOME}/cyverse_tmp/logs
+TMP_LOCATION=${HOME}/cyverse_tmp/staging
+
+# Just tests to ensure that the following commands are installed
+# Basically checking if IRODS is installed
+test_exists_executable() {
+    file=$FILE
+    if [[ -x "$file" ]]
+    then
+        :
+    else
+        echo "File '$file' is not executable or found"
+        exit 1
+    fi
+}
+
+
+FILE=/bin/date
+test_exists_executable
+
+FILE=/bin/mkdir
+test_exists_executable
+
+FILE=/bin/mv
+test_exists_executable
+
+FILE=$FIND
+test_exists_executable
+
+FILE=$IRSYNC
+test_exists_executable
+
+# determine the date
+tmp_d=`/bin/date +"%Y_%m_%d"`
+
+# determine log and create directory
+LOG=${LOG_LOCATION}/$0-${tmp_d}.log
+
+#/bin/mkdir $TMP_LOCATION/tmp
+# ensure the completed directory exists
+/bin/mkdir -p $DONE_LOCATION >>$LOG 2>&1
+
+
+target=$TMP_LOCATION
+if $FIND "$target" -mindepth 1 -print -quit | grep -q .; then
+    :
+else
+    echo -e `/bin/date`" Nothing to transfer\n" >>$LOG
+    exit 0
+fi
+
+
+#   Since folder is not empty we loop through folders in the folder
+#   And transfer the file using rysync
+for d in $TMP_LOCATION/?*/ ; do
+    #echo $d
+    $IRSYNC -r $d i:$DTL
+
+    # if sucessful then 
+    if [ $? == 0 ]; then
+        echo -e `/bin/date`" Transfer of $d sucessful, moving to completed \n" >>$LOG
+        /bin/mv $d $DONE_LOCATION 
+    fi
+done
+
+
---
+++ stage_to_cyverse.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# $1 = local path including file name (/local/path/to/filename.txt)
+# $2 = full remote path including file name (/iplant/home/user/path/to/file/filename.txt)
+if [ -z "$1" ]
+  then
+    echo "USAGE:"
+    echo ""
+    echo "stage_to_cyverse /local/path/to/filename.txt /iplant/home/user/path/to/file/filename.txt"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "USAGE:"
+    echo ""
+    echo "stage_to_cyverse /local/path/to/filename.txt /iplant/home/user/path/to/file/filename.txt"
+    exit 1
+fi
+
+
+# declare the locations of these executables and directories
+IPUT=/usr/bin/iput
+IRSYNC=/usr/bin/irsync
+TMP_LOCATION=${HOME}/cyverse_tmp/staging
+LOG_LOCATION=${HOME}/cyverse_tmp/logs
+
+
+# Just tests to ensure that the following commands are installed
+# Basically checking if IRODS is installed
+test_exists_executable() {
+    file=$FILE
+    if [[ -x "$file" ]]
+    then
+        :
+    else
+        echo "ERROR:"
+        echo "File '$file' is not executable or found"
+        exit 1
+    fi
+}
+
+
+FILE=/bin/date
+test_exists_executable
+
+FILE=/bin/mkdir
+test_exists_executable
+
+FILE=/bin/mv
+test_exists_executable
+
+FILE=/usr/bin/dirname
+test_exists_executable
+
+FILE=$IPUT
+test_exists_executable
+
+FILE=$IRSYNC
+test_exists_executable
+
+# determine the date
+tmp_d=`/bin/date +"%Y_%m_%d"`
+
+# determine log and create directory
+LOG=${LOG_LOCATION}/$0-${tmp_d}.log
+/bin/mkdir -p $LOG_LOCATION
+
+
+if [ ! -f $1 ]; then
+    echo "ERROR:"
+    echo "File not found!"
+    exit 1
+fi
+
+# attempt a file transfer
+echo `/bin/date`" attempting transfer of $1 to $2" >>$LOG
+`$IPUT -f -V $1 $2` >>$LOG 2>&1
+if [ $? != 0 ]; then
+
+	# at this point failure	
+	echo `/bin/date`" failed transfer" >>$LOG
+
+	# first create the path locally
+    tmp_path=`/usr/bin/dirname $2`
+
+    # create directory
+    full_path=$TMP_LOCATION/${tmp_d}${tmp_path}
+    /bin/mkdir -p ${full_path} >>$LOG 2>&1
+
+    # copy file
+	echo `/bin/date`" full path to file is ${full_path}, attempting copy" >>$LOG
+    /bin/mv $1 ${full_path} >>$LOG 2>&1
+fi
